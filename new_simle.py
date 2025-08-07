94c9419f24644b99db78e2eda8378372147b048f
yuri <yuri.durodie@gmail.com>
2025-08-07 09:54:37 +0200

    Advancing on correct training. Current problem the actor gradients are very small.

---
+++ new_simle.py
@@ -0,0 +1,18 @@
+from mpe2 import simple_push_v3
+from mpe2 import simple_v3
+
+env = simple_v3.env(render_mode="human")
+env.reset(seed=42)
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+
+    if termination or truncation:
+        action = None
+    else:
+        # this is where you would insert your policy
+        action = env.action_space(agent).sample()
+
+    env.step(action)
+
+env.close()
